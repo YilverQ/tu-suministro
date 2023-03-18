0057b30f3f96313651cc84a72413c26a9aa843b1
YilverQ <yilver0906@gmail.com>
2023-03-18 10:07:28 -0400

    Script para enviar correo fue modificado

--- correo.php
+++ correo.php
@@ -1,9 +1,10 @@
 <?php  
 
 	//Correos al que llegarán los mensajes.
+	#$destinatario = "yilver0906@gmail.com";
 	$destinatario = "tusuministrovzla@gmail.com";
 	$destinatario2 = "tusuministro@hotmail.com";
-	//$destinatario = "yilver0906@gmail.com";
+
 	
 	//Variables
 	$nombre	 = $_POST["nombre"];
@@ -12,14 +13,19 @@
 	$mensaje = $_POST["mensaje"];
 	$asunto = "Contacto Desde La Web";
 
+
 	//Carta
 	$carta 	= "De: " . $nombre . "\n";
 	$carta	= $carta . "Correo: " . $correo . "\n";
 	$carta	= $carta . "Teléfono: " . $telefono . "\n";
 	$carta  = $carta . "Mensaje: " . $mensaje . "\n";
 
+
 	mail($destinatario, $asunto, $carta);
-	mail($destinatario2, $asunto, $carta);
-	echo "<script>alert('correo enviado exitosamente.')</script>";
-	echo "<script>setTimeout(\"location.href='contactanos.php'\",1000)</script>";
+	mail($destinatario2, $asunto, $carta);	
+
+
+	#Redireccionar una a web
+	header("Location: https://www.tusuministro.com.ve/contactanos.php", TRUE, 302);
+	die();
 ?>
